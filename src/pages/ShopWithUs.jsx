887576b7c9b96279da9d17b4e5b46d879ea2740a
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-04-21 23:16:26 +0100

    Added Why Section

---
+++ src/pages/ShopWithUs.jsx
@@ -0,0 +1,52 @@
+import bestQuality from "../assets/img/best_quality.svg";
+import fastDelivery from "../assets/img/fast_delivery.svg";
+import freeShipping from "../assets/img/free_shipping.svg";
+
+const WhySection = () => {
+	return (
+		<section className="why_section layout_padding">
+			<div className="container">
+				<div className="heading_container heading_center">
+					<h2>Why Shop With Us</h2>
+				</div>
+				<div className="row">
+					<div className="col-md-4">
+						<div className="box ">
+							<div className="img-box">
+								<img src={fastDelivery} width={"20%"} alt="" />
+							</div>
+							<div className="detail-box">
+								<h5>Fast Delivery</h5>
+								<p>variations of passages of Lorem Ipsum available</p>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-4">
+						<div className="box ">
+							<div className="img-box">
+								<img src={freeShipping} width={"20%"} alt="" />
+							</div>
+							<div className="detail-box">
+								<h5>Free Shiping</h5>
+								<p>variations of passages of Lorem Ipsum available</p>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-4">
+						<div className="box ">
+							<div className="img-box">
+								<img src={bestQuality} width={"20%"} alt="" />
+							</div>
+							<div className="detail-box">
+								<h5>Best Quality</h5>
+								<p>variations of passages of Lorem Ipsum available</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default WhySection;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,11 @@
 import Hero from "../pages/Hero";
+import WhySection from "../pages/ShopWithUs";
 
 const HomeScreen = () => {
 	return (
 		<>
 			<Hero />
+			<WhySection />
 		</>
 	);
 };
