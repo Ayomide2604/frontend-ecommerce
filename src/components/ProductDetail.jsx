d69dffb7808c90b6ac6dcd7665a755beb398f923
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-04-22 01:17:06 +0100

    enhanced ui of product detail screen

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -14,25 +14,22 @@ const ProductDetail = () => {
 					<div className="col-md-6">
 						<div className="item-entry">
 							<a href="#" className="product-item md-height bg-gray d-block">
-								<img src={product.image} alt="Image" className="img-fluid" />
+								<img src={product.image} alt="Image" className="img-fluid mb-5" />
 							</a>
 						</div>
 					</div>
-					<div className="col-md-6 ">
+					<div className="col-md-6  ">
 						<h2 className="text-black">{product.title}</h2>
 
 						<p className="mb-4">{product.description}</p>
 						<p>
-							<strong className="text-primary h4">${product.price}</strong>
+							<strong className=" h4">${product.price}</strong>
 						</p>
 
 						<div className="mb-5">
 							<div className="input-group mb-3" style={{ maxWidth: "120px" }}>
 								<div className="input-group-prepend">
-									<button
-										className="btn btn-outline-primary js-btn-minus"
-										type="button"
-									>
+									<button className="btn btn-outline-dark " type="button">
 										-
 									</button>
 								</div>
@@ -45,20 +42,14 @@ const ProductDetail = () => {
 									aria-describedby="button-addon1"
 								/>
 								<div className="input-group-append">
-									<button
-										className="btn btn-outline-primary js-btn-plus"
-										type="button"
-									>
+									<button className="btn btn-outline-dark " type="button">
 										+
 									</button>
 								</div>
 							</div>
 						</div>
 						<p className="btn-box d-flex justify-content-start">
-							<a
-								href="cart.html"
-								className="buy-now btn btn-sm height-auto px-4 py-3 btn1"
-							>
+							<a href="cart.html" className=" btn btn1 rounded-4">
 								Add To Cart
 							</a>
 						</p>
